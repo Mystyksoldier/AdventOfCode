82611bc7b731b5bbbaebe6021eb4c3358308fe5e
Dave Havelaar <91124031+Mystyksoldier@users.noreply.github.com>
2023-01-14 17:22:28 +0100

    Add files via upload

---
+++ AdventOfCode/Day 1/Count.php
@@ -0,0 +1,29 @@
+<?php
+$input = file_get_contents('elfs.txt');
+$inputArr = preg_split('/\r\n|\r|\n/', $input);
+$finalArr = array();
+$subArr = array();
+foreach($inputArr as $val){
+    if(!empty($val)){
+        array_push($subArr, $val);
+    }
+    else{
+        array_push($finalArr, $subArr);
+        $subArr = array();
+    }
+}
+array_push($finalArr, $subArr);
+
+$maxCalories = 0;
+$maxElf = 0;
+
+for ($i = 0; $i < count($finalArr); $i++) {
+    $currentElfCalories = array_sum($finalArr[$i]);
+    if ($currentElfCalories > $maxCalories) {
+        $maxCalories = $currentElfCalories;
+        $maxElf = $i;
+    }
+}
+
+echo "$maxCalories";
+?>
